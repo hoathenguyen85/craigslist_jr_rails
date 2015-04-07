3b10dda282f640093c8c7264e00e78b478f9aae7
Hoa Nguyen & Kiran <pair+hoathenguyen85.phanendar@devbootcamp.com>
2015-04-07 16:34:34 -0400

    adding user authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,17 +1,23 @@
 class AuthenticationController < ApplicationController
 
+  # this is the controller for the GET signup
   def index
+
   end
 
+
+  # this is the controller for the POST signup
   def create
    user = User.find_by(email: params[:email])
-   if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect user_path(user)
-  else
-    session[:message] = "invalid username and/or password"
-    redirect '/?error=true'
-  end
-end
 
+   p user
+   p params[:email]
+   p params
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to authentication_index_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
 
+  # This is the controller for the signin
   def new
+    @user = User.new
   end
 
+  def show
+  end
+
+  # This is the controller fot the signin for the post
   def create
+    if user_params[:password] == user_params[:password_digest]
     @user = User.new(user_params)
+  end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password, :password_digest)
   end
 end
